2cfadd37c9746a699a493f69dbb1f67bc292f66d
ᏗᎮ๏ɭɭ๏ <72794063+PmsHere@users.noreply.github.com>
2022-12-06 10:28:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,7 +25,7 @@ USE_CAPTION_FILTER = bool(environ.get("USE_CAPTION_FILTER", False))
 PICS = (
     environ.get(
         "PICS",
-        "https://telegra.ph/file/7e56d907542396289fee4.jpg https://telegra.ph/file/9aa8dd372f4739fe02d85.jpg https://telegra.ph/file/adffc5ce502f5578e2806.jpg https://telegra.ph/file/6937b60bc2617597b92fd.jpg https://telegra.ph/file/09a7abaab340143f9c7e7.jpg https://telegra.ph/file/5a82c4a59bd04d415af1c.jpg https://telegra.ph/file/323986d3bd9c4c1b3cb26.jpg https://telegra.ph/file/b8a82dcb89fb296f92ca0.jpg https://telegra.ph/file/31adab039a85ed88e22b0.jpg https://telegra.ph/file/c0e0f4c3ed53ac8438f34.jpg https://telegra.ph/file/eede835fb3c37e07c9cee.jpg https://telegra.ph/file/e17d2d068f71a9867d554.jpg https://telegra.ph/file/8fb1ae7d995e8735a7c25.jpg https://telegra.ph/file/8fed19586b4aa019ec215.jpg https://telegra.ph/file/8e6c923abd6139083e1de.jpg https://telegra.ph/file/0049d801d29e83d68b001.jpg",
+        "https://te.legra.ph/file/9360577ec15069961e9e9.jpg https://te.legra.ph/file/2b8c4abd33ab417ddfbd1.jpg https://te.legra.ph/file/04b10b450de3e8daea891.jpg https://te.legra.ph/file/27c3b2fb4f953725d1633.jpg ",
     )
 ).split()
 
