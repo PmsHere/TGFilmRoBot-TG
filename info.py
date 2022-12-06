b9008f542577ca7c5380e9beffa7c7e58c9ad4d6
ᏗᎮ๏ɭɭ๏ <72794063+PmsHere@users.noreply.github.com>
2022-12-06 08:01:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,10 +15,10 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get("SESSION", "Media_search")
-API_ID = int(environ["API_ID"])
-API_HASH = environ["API_HASH"]
-BOT_TOKEN = environ["BOT_TOKEN"]
-USER_SESSION = environ["USER_SESSION"]
+API_ID = environ.get('API_ID', '4534665')
+API_HASH = environ.get('API_HASH', '366f7a24d64c5d6578df57e3b5b03fa0')
+BOT_TOKEN = environ.get('BOT_TOKEN', '')
+USER_SESSION = environ.get('USER_SESSION', '')
 # Bot settings
 CACHE_TIME = int(environ.get("CACHE_TIME", 300))
 USE_CAPTION_FILTER = bool(environ.get("USE_CAPTION_FILTER", False))
@@ -32,19 +32,19 @@ PICS = (
 # Admins, Channels & Users
 ADMINS = [
     int(admin) if id_pattern.search(admin) else admin
-    for admin in environ.get("ADMINS", "").split()
+    for admin in environ.get('ADMINS', '1893684647').split()
 ]
 CHANNELS = [
     int(ch) if id_pattern.search(ch) else ch
-    for ch in environ.get("CHANNELS", "0").split()
+    for ch in environ.get('CHANNELS', '-1001682880445').split()
 ]
 auth_users = [
     int(user) if id_pattern.search(user) else user
-    for user in environ.get("AUTH_USERS", "").split()
+    for user in environ.get('AUTH_USERS', '').split()
 ]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get("AUTH_CHANNEL")
-auth_grp = environ.get("AUTH_GROUP")
+auth_channel = environ.get('AUTH_CHANNEL', '-1001736005739')
+auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = (
     int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 )
@@ -52,14 +52,14 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get("DATABASE_URI", "")
-DATABASE_NAME = environ.get("DATABASE_NAME", "Rajappan")
+DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
 COLLECTION_NAME = environ.get("COLLECTION_NAME", "Telegram_files")
 
 # Others
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", 0))
-SUPPORT_CHAT = environ.get("SUPPORT_CHAT", "TeamEvamaria")
-P_TTI_SHOW_OFF = is_enabled((environ.get("P_TTI_SHOW_OFF", "False")), False)
-IMDB = is_enabled((environ.get("IMDB", "True")), True)
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", '-1001390229228'))
+SUPPORT_CHAT = environ.get("SUPPORT_CHAT", "MalluTorentzTG")
+P_TTI_SHOW_OFF = is_enabled((environ.get("P_TTI_SHOW_OFF", "True")), True)
+IMDB = is_enabled((environ.get("IMDB", "False")), False)
 SINGLE_BUTTON = is_enabled((environ.get("SINGLE_BUTTON", "False")), False)
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", None)
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
