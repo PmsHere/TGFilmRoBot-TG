a53516d2a78ccc3e0b0797466d0ec11d095556a8
PmsHere <72794063+PmsHere@users.noreply.github.com>
2023-06-18 14:47:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,4 +1,9 @@
-id_pattern = re.compile(r'^.\d+$')
+import re
+from os import environ
+
+id_pattern = re.compile(r"^.\d+$")
+
+
 def is_enabled(value, default):
     if value.lower() in ["true", "yes", "1", "enable", "y"]:
         return True
@@ -7,58 +12,105 @@ def is_enabled(value, default):
     else:
         return default
 
-# Bot information
-SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '4534665'))
-API_HASH = environ.get('API_HASH', '366f7a24d64c5d6578df57e3b5b03fa0')
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
 
+# Bot information
+SESSION = environ.get("SESSION", "Media_search")
+API_ID = environ.get('API_ID', '2961089')
+API_HASH = environ.get('API_HASH', '0eb4af8d3b257fd32cd7daf93274850f')
+BOT_TOKEN = environ.get('BOT_TOKEN', '1953545966:AAEoURhpLegixvmBmtk5-nbredCMvlym4h0')
+USER_SESSION = environ.get('USER_SESSION', '')
 # Bot settings
-CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/8619a6f258621134b7576.jpg https://telegra.ph/file/d8daf35960bbb4a7f8558.jpg')).split()
+CACHE_TIME = int(environ.get("CACHE_TIME", 300))
+USE_CAPTION_FILTER = bool(environ.get("USE_CAPTION_FILTER", False))
+PICS = (
+    environ.get(
+        "PICS",
+        "",
+    )
+).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1893684647').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001682880445').split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+ADMINS = [
+    int(admin) if id_pattern.search(admin) else admin
+    for admin in environ.get('ADMINS', '956832324 820786059').split()
+]
+CHANNELS = [
+    int(ch) if id_pattern.search(ch) else ch
+    for ch in environ.get('CHANNELS', '-1001505754602 -1001242705965').split()
+]
+auth_users = [
+    int(user) if id_pattern.search(user) else user
+    for user in environ.get('AUTH_USERS', '').split()
+]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL')
-auth_grp = environ.get('AUTH_GROUP')
-AUTH_CHANNEL = environ.get('AUTH_CHANNEL','-1001830483996')
+auth_channel = environ.get('AUTH_CHANNEL', '-1001715035701')
+auth_grp = environ.get('AUTH_GROUP', '-1001258893901 -1001164202707 -1001702528738 -1001437868731 -1001449723794 -1001553701267 -1001449723794 -1001507746039 -1001738042448 -1001687626810 -1001696385814 -1001987580262')
+AUTH_CHANNEL = (
+    int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
+)
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://abcd:abcd@cluster0.er8cj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
+COLLECTION_NAME = environ.get("COLLECTION_NAME", "Tg_movie_filescb")
 
-# Others
-
+# Others 
 DELETE_TIME = int(environ.get('DELETE_TIME', '300'))
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001390229228'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'MalluTorentzTG')
-P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', 'True')), False)
-IMDB = is_enabled((environ.get('IMDB', 'False')), True)
-SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', 'True')), False)
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", '<pre>{file_name}</pre>\n\n<b>🔗𝗖𝗛𝗔𝗡𝗡𝗘𝗟 𝗟𝗜𝗡𝗞☞\n\nhttps://t.me/+WdzjOMj3tVY0YjJk\nhttps://t.me/+WdzjOMj3tVY0YjJk\nhttps://t.me/+WdzjOMj3tVY0YjJk</b>')
-BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", '<pre>{file_name}</pre>\n\n<b>🔗𝗖𝗛𝗔𝗡𝗡𝗘𝗟 𝗟𝗜𝗡𝗞☞\n\nhttps://t.me/+WdzjOMj3tVY0YjJk\nhttps://t.me/+WdzjOMj3tVY0YjJk\nhttps://t.me/+WdzjOMj3tVY0YjJk</b>')
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "🏷 𝖳𝗂𝗍𝗅𝖾: <a href={url}>{title}</a> \n🔮 𝖸𝖾𝖺𝗋: {year} \n⭐️ 𝖱𝖺𝗍𝗂𝗇𝗀𝗌: {rating}/ 10  \n🎭 𝖦𝖾𝗇𝖾𝗋𝗌: {genres} \n\n🎊 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 [Mallu Torentz](https://t.me/+WdzjOMj3tVY0YjJk)")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", '-1001426161220'))
+SUPPORT_CHAT = environ.get("SUPPORT_CHAT", "MalluTorentzTG")
+P_TTI_SHOW_OFF = is_enabled((environ.get("P_TTI_SHOW_OFF", "True")), True)
+IMDB = is_enabled((environ.get("IMDB", "False")), False)
+SINGLE_BUTTON = is_enabled((environ.get("SINGLE_BUTTON", "True")), True)
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", '<pre>{file_name}</pre>\n\n<b>╔══ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ═══╗\n♻️ 𝙅𝙊𝙄𝙉 :- @MalluTorentzTG\n♻️ 𝙅𝙊𝙄𝙉 :- @MalluTorentGroup\n♻️ 𝙅𝙊𝙄𝙉 :- @MT_LinkTG\n╚══ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ═══╝</b>')
+BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
+IMDB_TEMPLATE = environ.get(
+    "IMDB_TEMPLATE",
+    "<b>Query: {query}</b> \n‌‌‌‌IMDb Data:\n\n🏷 Title: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating}</a> / 10",
+)
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
-INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
-MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
-PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
-PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), True)
+INDEX_REQ_CHANNEL = int(environ.get("INDEX_REQ_CHANNEL", LOG_CHANNEL))
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get("FILE_STORE_CHANNEL", "-1001658035444")).split()]
+MELCOW_NEW_USERS = is_enabled((environ.get("MELCOW_NEW_USERS", "True")), True)
+PROTECT_CONTENT = is_enabled((environ.get("PROTECT_CONTENT", "False")), False)
+PUBLIC_FILE_STORE = is_enabled((environ.get("PUBLIC_FILE_STORE", "True")), True)
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
-LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
-LOG_STR += ("P_TTI_SHOW_OFF found , Users will be redirected to send /start to Bot PM instead of sending file file directly\n" if P_TTI_SHOW_OFF else "P_TTI_SHOW_OFF is disabled files will be send in PM, instead of sending start.\n")
-LOG_STR += ("SINGLE_BUTTON is Found, filename and files size will be shown in a single button instead of two separate buttons\n" if SINGLE_BUTTON else "SINGLE_BUTTON is disabled , filename and file_sixe will be shown as different buttons\n")
-LOG_STR += (f"CUSTOM_FILE_CAPTION enabled with value {CUSTOM_FILE_CAPTION}, your files will be send along with this customized caption.\n" if CUSTOM_FILE_CAPTION else "No CUSTOM_FILE_CAPTION Found, Default captions of file will be used.\n")
-LOG_STR += ("Long IMDB storyline enabled." if LONG_IMDB_DESCRIPTION else "LONG_IMDB_DESCRIPTION is disabled , Plot will be shorter.\n")
-LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies if movie not found\n" if SPELL_CHECK_REPLY else "SPELL_CHECK_REPLY Mode disabled\n")
-LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
+LOG_STR += (
+    "IMDB Results are enabled, Bot will be showing imdb details for you queries.\n"
+    if IMDB
+    else "IMBD Results are disabled.\n"
+)
+LOG_STR += (
+    "P_TTI_SHOW_OFF found , Users will be redirected to send /start to Bot PM instead of sending file file directly\n"
+    if P_TTI_SHOW_OFF
+    else "P_TTI_SHOW_OFF is disabled files will be send in PM, instead of sending start.\n"
+)
+LOG_STR += (
+    "SINGLE_BUTTON is Found, filename and files size will be shown in a single button instead of two separate buttons\n"
+    if SINGLE_BUTTON
+    else "SINGLE_BUTTON is disabled , filename and file_sixe will be shown as different buttons\n"
+)
+LOG_STR += (
+    f"CUSTOM_FILE_CAPTION enabled with value {CUSTOM_FILE_CAPTION}, your files will be send along with this customized caption.\n"
+    if CUSTOM_FILE_CAPTION
+    else "No CUSTOM_FILE_CAPTION Found, Default captions of file will be used.\n"
+)
+LOG_STR += (
+    "Long IMDB storyline enabled."
+    if LONG_IMDB_DESCRIPTION
+    else "LONG_IMDB_DESCRIPTION is disabled , Plot will be shorter.\n"
+)
+LOG_STR += (
+    "Spell Check Mode Is Enabled, bot will be suggesting related movies if movie not found\n"
+    if SPELL_CHECK_REPLY
+    else "SPELL_CHECK_REPLY Mode disabled\n"
+)
+LOG_STR += (
+    f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n"
+    if MAX_LIST_ELM
+    else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n"
+)
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
