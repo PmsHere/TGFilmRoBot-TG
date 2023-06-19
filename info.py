29a612b0c073897d503488dba7d6cce88061a201
PmsHere <72794063+PmsHere@users.noreply.github.com>
2023-06-19 17:08:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,9 +51,9 @@ AUTH_CHANNEL = (
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://abcd:abcd@cluster0.er8cj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://prince:abcd@cluster0.jr92hnw.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
-COLLECTION_NAME = environ.get("COLLECTION_NAME", "Tg_movie_filescb")
+COLLECTION_NAME = environ.get("COLLECTION_NAME", "Telegram_files")
 
 # Others 
 DELETE_TIME = int(environ.get('DELETE_TIME', '300'))
