3614fd27441c77c7077251990d0b42302a90a03e
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2023-06-25 15:32:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -634,3 +634,38 @@ async def save_template(client, message):
     template = message.text.split(" ", 1)[1]
     await save_group_settings(grp_id, "template", template)
     await sts.edit(f"Successfully changed template for {title} to\n\n{template}")
+
+
+ 
+
+@Client.on_message(filters.command("getlink") & filters.incoming)
+def get_link(client, message):
+    # Check if the message has a parameter
+    if len(message.command) < 2:
+        message.reply_text("Please provide a chat ID.")
+        return
+
+    # Get the chat ID from the command
+    chat_id = message.command[1]
+
+    try:
+        # Get information about the chat
+        chat = client.get_chat(chat_id)
+
+        invite_link = client.export_chat_invite_link(chat_id)
+        if invite_link:
+            message.reply_text(f"Invite link for chat {chat.title}: {invite_link}")
+        else:
+            invite_link = client.create_chat_invite_link(chat_id)
+            message.reply_text(f"Invite link created for chat {chat.title}: {invite_link.link}")
+
+        # Retrieve the list of administrators
+        admins = client.get_chat_members(chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS)
+        admin_list = "\n".join([f"{admin.user.first_name} (@{admin.user.username}) [{admin.user.id}]" for admin in admins])
+        message.reply_text(f"Administrators in chat {chat.title}:\n{admin_list}")
+
+    except Exception as e:
+        message.reply_text(f"Error: {e}")
+
+
+
