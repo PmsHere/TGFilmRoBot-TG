e4dfc7cb5622d918a8f262e2b4b60ae48e258630
PmsHere <72794063+PmsHere@users.noreply.github.com>
2024-09-01 19:56:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ AUTH_CHANNEL = (
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://apollo:pmshere@cluster0.0gkxnrn.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://pmshere:reK96UvVes8weA3C@cluster0.mp7z3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
 COLLECTION_NAME = environ.get("COLLECTION_NAME", "Telegram_files")
 
